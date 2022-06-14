e069cb6c19a85a549b314aac8648f2cf020157b3
Tunde Alao <46252854+TundeAlao@users.noreply.github.com>
2022-06-14 01:55:58 +0100

    init

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<sift_flutter/SiftFlutterPlugin.h>)
+#import <sift_flutter/SiftFlutterPlugin.h>
+#else
+@import sift_flutter;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SiftFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"SiftFlutterPlugin"]];
+}
+
+@end
