3adc39b250c09d28b032070a284d3b84d0e035e8
Tunde Alao <46252854+TundeAlao@users.noreply.github.com>
2022-06-14 02:51:16 +0100

    Impl Android method call handlers

--- android/src/main/java/com/sift/sift_flutter/SiftFlutterPlugin.java
+++ android/src/main/java/com/sift/sift_flutter/SiftFlutterPlugin.java
@@ -7,9 +7,10 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import siftscience.android.Sift;
 
 /** SiftFlutterPlugin */
-public class SiftFlutterPlugin implements FlutterPlugin, MethodCallHandler {
+public class SiftFlutterPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware{
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
@@ -22,11 +23,38 @@ public class SiftFlutterPlugin implements FlutterPlugin, MethodCallHandler {
     channel.setMethodCallHandler(this);
   }
 
+  public void openSift(String[] siftData) {
+    Sift.open(this, new Sift.Config.Builder()
+            .withAccountId(siftData[0])
+            .withBeaconKey(siftData[1])
+            .withDisallowLocationCollection(true)
+            .build());
+    Sift.setUserId(siftData[2]);
+    Sift.collect();
+  }
+
+  public void unSetUserID() {
+      Sift.unsetUserId();
+      Sift.collect();
+  }
+
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     if (call.method.equals("getPlatformVersion")) {
       result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
+    } if else (call.method.equals("setUserID")) {
+      String siftDataStr = call.arguments.toString();
+      Log.d("sift data string", siftDataStr);
+      siftDataStr = siftDataStr.replace("[", "");
+      siftDataStr = siftDataStr.replace("]", "");
+      siftDataStr = siftDataStr.replace(" ", "");
+      String[] siftData = siftDataStr.split(",");
+      openSift(siftData);
+      result.success("Success set UserID!!!");
+  } else if (call.method.equals("unsetUserID")) {
+      unSetUserID();
+      result.success("Success unset User!!!");
+  } else {
       result.notImplemented();
     }
   }
